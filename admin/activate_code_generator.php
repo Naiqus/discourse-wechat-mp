6f2ff0f12f96883106311960d2772810d02df029
Suqian Zhang <suqian.zhang@rwth-aachen.de>
2015-01-02 22:23:35 +0100

    For Admin: Wechat activition coder generator

---
+++ admin/activate_code_generator.php
@@ -0,0 +1,115 @@
+<html>
+  <head>
+    <title>Wechat Activation Code Generator</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+  </head>
+  <body>
+    <?php
+      // define variables and set to empty values
+      $username = $api = $userbindingkey = "";
+      $usernameErr = $apiErr = $userbindingkeyErr = "";
+
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["username"])) {
+        $usernameErr = "Username 是必填的";
+        } else {
+          $username = test_input($_POST["username"]);
+        }
+        
+        if (empty($_POST["api"])) {
+          $apiErr = "API 是必填的";
+        } else {
+          $api = test_input($_POST["api"]);
+        }
+          
+        if (empty($_POST["userbindingkey"])) {
+          $userbindingkeyErr = "用户绑定Key 是必填的";
+        } else {
+          $userbindingkey = test_input($_POST["userbindingkey"]);
+        }
+      }
+    ?>
+  <div class="container">
+    <div class="row">
+      <div class="center-block col-md-5">
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" class="form-horizontal">
+        <fieldset>
+
+        <!-- Form Name -->
+        <legend>东一区微信绑定管理</legend>
+
+        <!-- Text input-->
+        <div class="control-group">
+          <label class="control-label" for="username">用户名</label>
+          <div class="controls">
+            <input id="username" name="username" type="text" placeholder="输入用户名" class="input-xlarge">
+            <span class="error">* <?php echo $usernameErr;?></span>
+          </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="control-group">
+          <label class="control-label" for="api">输入API_key</label>
+          <div class="controls">
+            <input id="api" name="api" type="text" placeholder="API_key" class="input-xlarge">
+            <span class="error">* <?php echo $apiErr;?></span>
+          </div>
+        </div>
+
+        <!-- Text input-->
+        <div class="control-group">
+          <label class="control-label" for="userbindingkey">用户绑定Key</label>
+          <div class="controls">
+            <input id="userbindingkey" name="userbindingkey" type="text" placeholder="八位绑定key" class="input-xlarge">
+            <span class="error">* <?php echo $userbindingkeyErr;?></span>
+            <p class="help-block">API_key的最后8位</p>
+          </div>
+        </div>
+
+        <!-- Button (Double) -->
+        <div class="control-group">
+          <label class="control-label" for="confirm"></label>
+          <div class="controls">
+            <button id="confirm" name="confirm" class="btn btn-success" type="submit">确定</button>
+            <button id="cancel" name="cancel" class="btn btn-danger">取消</button>
+          </div>
+        </div>
+
+        </fieldset>
+        </form>
+      </div>
+    </div>
+  </div>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+  <?php
+      function test_input($data) {    //preprocess input
+         $data = trim($data);
+         $data = stripslashes($data);
+         $data = htmlspecialchars($data);
+         return $data;
+      }
+
+        $db_servername = "YourMySQLHost";
+        $db_username = "YourUsername";
+        $db_password = "YourPassword";
+
+      if(!empty($username)&&!empty($api)&&!empty($userbindingkey)){
+        $connection = mysql_connect($db_servername, $db_username, $db_password) OR DIE ("Unable to connect to database! Please try again later.");
+        if (!$connection){
+          echo "<div class=\"alert alert-danger\">服务器异常，请稍后重试。</div>";
+        }else{
+              mysql_select_db("discourseUsers");
+              $sql = "INSERT INTO `account_binding` (`username`, `API_key`, `index`, `openID`) VALUES ('".$username."', '".$api."', '".$userbindingkey."', NULL);";            
+              if (mysql_query($sql)) {
+                  echo "<div class=\"alert alert-success\">用户:".$username."添加成功\n 请将用户绑定key：<b>".$userbindingkey."</b> 私信给用户</div>";
+              } else {
+                  echo "<div class=\"alert alert-danger\">用户添加失败，请稍后重试</div>";
+              }
+              mysql_close($connection);
+        }
+      }
+  ?>
+
+  </body>
+</html>
